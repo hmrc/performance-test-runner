0c072e072d4dafd059ea96b1fafa6d17ac4fa3e7
Ovidiu Bura <Ovidiu.Bura@digital.hmrc.gov.uk>
2015-11-11 16:55:39 +0000

    Add runLocal parameter and load the services-local file only when running locally

--- src/main/scala/uk/gov/hmrc/performance/conf/Configuration.scala
+++ src/main/scala/uk/gov/hmrc/performance/conf/Configuration.scala
@@ -16,14 +16,36 @@
 
 package uk.gov.hmrc.performance.conf
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.{Config, ConfigFactory, ConfigMergeable}
 
 import scala.util.Properties
 
 
 trait Configuration {
+
   import scala.collection.JavaConverters._
-  private val applicationConfig: Config = ConfigFactory.systemProperties().withFallback(ConfigFactory.load())
+
+  private val defaultConfig = ConfigFactory.systemProperties().withFallback(ConfigFactory.load())
+
+  private val localConfig: ConfigMergeable =
+    try {
+      ConfigFactory.load("services-local")
+    } catch {
+      case _: Exception => throw new RuntimeException("Couldn't load services-local.conf. Please check the configuration file")
+    }
+
+  lazy val runLocal: Boolean =
+    try {
+      defaultConfig.getBoolean("runLocal")
+    } catch {
+      case _: Exception => throw new RuntimeException("Couldn't load the value runLocal. Please check the runLocal value in the application.conf file")
+    }
+
+  lazy val applicationConfig: Config = {
+    if (runLocal)
+      defaultConfig.withFallback(localConfig)
+    else defaultConfig
+  }
 
   def readProperty(property: String): String = {
     Properties.propOrElse(property, applicationConfig.getString(property))
