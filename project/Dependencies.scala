fa23776d5504206860de7ce46dc7fac02fd390a9
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-02-23 12:24:35 +0000

    Updating plugin versions and organising dependencies before Gatling upgrade. Removed resolvers from build.sbt as they should come from sbt-auto-build plugin.

---
+++ project/Dependencies.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+object Dependencies {
+
+  val compile =
+    Seq(
+      "uk.gov.hmrc" %% "logback-json-logger" % "4.2.0",
+      "com.typesafe.play" %% "play-json" % "2.6.10"
+    )
+  val test =
+    Seq("org.scalatest" %% "scalatest" % "3.0.5" % Test,
+      "org.pegdown" % "pegdown" % "1.6.0" % Test)
+
+}
