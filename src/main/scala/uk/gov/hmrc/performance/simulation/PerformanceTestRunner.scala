1d406e64bfdc512aba59b4234f865a6aac92e11e
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-02-25 12:29:31 +0000

    PLATUI-962: Version 3.4.2 does not allow starting simulations from a test directly. To address this JourneySetup has been extracted from PerformanceTestRunner.scala. PerformanceTestRunnerSpec.scala has been testing only the JourneySetup rather than the Gatling simulation. So this is renamed to JourneySetupSpec. Added Scaladoc for the JourneySetup trait.

--- src/main/scala/uk/gov/hmrc/performance/simulation/PerformanceTestRunner.scala
+++ src/main/scala/uk/gov/hmrc/performance/simulation/PerformanceTestRunner.scala
@@ -17,90 +17,9 @@
 package uk.gov.hmrc.performance.simulation
 
 import io.gatling.core.Predef._
-import io.gatling.core.structure.{PopulationBuilder, ScenarioBuilder}
-import uk.gov.hmrc.performance.conf.{HttpConfiguration, JourneyConfiguration, PerftestConfiguration}
-import uk.gov.hmrc.performance.feeder.CsvFeeder
+import uk.gov.hmrc.performance.conf.HttpConfiguration
 
-import scala.util.Random
-
-trait PerformanceTestRunner
-    extends Simulation
-    with HttpConfiguration
-    with JourneyConfiguration
-    with PerftestConfiguration {
-
-  private[simulation] val parts = scala.collection.mutable.MutableList[JourneyPart]()
-
-  def setup(id: String, description: String): JourneyPart = {
-
-    val part: JourneyPart = JourneyPart(id, description)
-    parts += part
-    part
-  }
-
-  private def journeys: Seq[Journey] = {
-
-    println(s"Implemented journey parts: ${parts.map(_.id).mkString(", ")}")
-
-    definitions(labels).map { conf =>
-      println(s"Setting up scenario '${conf.id}' to run at ${conf.load} JPS and load to $loadPercentage %")
-
-      val partsInJourney = conf.parts.map(p =>
-        parts
-          .find(_.id.trim == p.trim)
-          .getOrElse(
-            throw new IllegalArgumentException(
-              s"Scenario '${conf.id}' is configured to run '$p' but there is no journey part for it in the code"
-            )
-          )
-      )
-
-      val first = group(partsInJourney.head.description) {
-        partsInJourney.head.builder
-      }
-      val chain = partsInJourney.tail.foldLeft(first)((c, p) =>
-        c.group(p.description) {
-          p.builder
-        }
-      )
-
-      new Journey {
-
-        lazy val feeder = conf.feeder
-        val RNG         = new Random
-
-        override lazy val builder: ScenarioBuilder = {
-          val scenarioBuilder =
-            if (!feeder.isEmpty) scenario(conf.description).feed(new CsvFeeder(feeder))
-            else scenario(conf.description)
-          scenarioBuilder
-            .feed(Iterator.continually(Map("currentTime" -> System.currentTimeMillis().toString)))
-            .feed(Iterator.continually(Map("random" -> Math.abs(RNG.nextInt()))))
-            .exitBlockOnFail(exec(chain))
-        }
-
-        override lazy val load: Double = conf.load
-      }
-
-    }
-  }
-
-  private def withAtLeasOneRequestInTheFullTest(load: Double) = load match {
-    case rate if (constantRateTime.toSeconds * rate).toInt < 1 => 1d / (constantRateTime.toSeconds - 1)
-    case rate                                                  => rate
-  }
-
-  private def withInjectedLoad(journeys: Seq[Journey]): Seq[PopulationBuilder] = journeys.map { scenario =>
-    val load = withAtLeasOneRequestInTheFullTest(scenario.load * loadPercentage)
-
-    val injectionSteps = List(
-      rampUsersPerSec(noLoad).to(load).during(rampUpTime),
-      constantUsersPerSec(load).during(constantRateTime),
-      rampUsersPerSec(load).to(noLoad).during(rampDownTime)
-    )
-
-    scenario.builder.inject(injectionSteps)
-  }
+trait PerformanceTestRunner extends Simulation with HttpConfiguration with JourneySetup {
 
   def runSimulation(): Unit = {
 
