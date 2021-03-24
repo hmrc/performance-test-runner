ea1b89699613040e02c3228f894632284d4d55fc
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-03-24 16:58:56 +0000

    PLATUI-969: Add ScalaDocs for PerformanceTestRunner

--- src/main/scala/uk/gov/hmrc/performance/simulation/PerformanceTestRunner.scala
+++ src/main/scala/uk/gov/hmrc/performance/simulation/PerformanceTestRunner.scala
@@ -20,10 +20,26 @@ import io.gatling.core.Predef._
 import org.slf4j.{Logger, LoggerFactory}
 import uk.gov.hmrc.performance.conf.HttpConfiguration
 
+/** Trait extending `io.gatling.core.scenario.Simulation`. Use within a performance test to set up Journeys and invoke
+  *  `runSimulation()`, a method to configure the Simulation setup.
+  */
+
 trait PerformanceTestRunner extends Simulation with HttpConfiguration with JourneySetup {
 
   private val logger: Logger = LoggerFactory.getLogger(classOf[PerformanceTestRunner])
 
+  /** Configures `io.gatling.core.scenario.Simulation.setUp`. This method is invoked from within a performance test.
+    *
+    * For smoke tests i.e when `uk.gov.hmrc.performance.conf.PerftestConfiguration.runSingleUserJourney`` is `true`,
+    * the setUp is configured to run only for 1 user.
+    *
+    * For a full test, the setUp is configured with:
+    *
+    *  - Journeys and the load to run
+    *  - Duration of the run
+    *  - Protocol configuration and
+    *  - Assertions
+    */
   def runSimulation(): Unit = {
 
     import scala.concurrent.duration._
